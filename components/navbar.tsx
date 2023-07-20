15ced49257cdb3ca2c1dc0f86c3be22973acc1d6
Pedro <pedrohalmeidamendonca@gmail.com>
2023-07-20 00:28:40 -0300

    First day building

---
+++ components/navbar.tsx
@@ -0,0 +1,21 @@
+import { Menu } from 'lucide-react';
+import { UserButton } from '@clerk/nextjs';
+
+import { Button } from '@/components/ui/button';
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center p-4">
+      <Button
+        variant="ghost"
+        size="icon"
+        className="md:hidden"
+      >
+        <Menu />
+      </Button>
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+}
